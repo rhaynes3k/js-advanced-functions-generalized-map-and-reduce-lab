d9852000eb5e978a0acdd07f24637d0477b0d336
rhaynes3k <rhaynes3k@gmail.com>
2021-06-18 03:28:12 -0500

    commit tree 80264ca18c1f2f6d44d48fbbdd30cd0b38207c4d

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+let map = function(srcArr, cb){
+    let newArr = []
+    srcArr.forEach(num => {
+        newArr.push(cb(num))
+    })
+    return newArr
+}
+
+let reduce = function(srcArr, sp = 0){
+  let tot = sp
+  srcArr.forEach(num =>{
+    tot = tot + num
+  })
+  return tot
+}
